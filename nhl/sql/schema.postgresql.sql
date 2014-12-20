800418ed555a149be92abf5c90ce4680b032e371
Kyle Dunn <kdunn926@gmail.com>
2014-12-19 19:48:00 -0700

    Adding NHL tools

---
+++ nhl/sql/schema.postgresql.sql
@@ -0,0 +1,185 @@
+set search_path to nhl;
+
+create schema nhl;
+
+drop table if exists nhl.teams cascade;
+drop table if exists nhl.games cascade;
+drop table if exists nhl.events cascade;
+drop table if exists nhl.events_players;
+drop table if exists nhl.events_penaltybox;
+
+drop table if exists nhl.players cascade;
+drop table if exists nhl.stats_skaters_summary;
+drop table if exists nhl.stats_skaters_timeonice;
+
+/* USED BY events.py */
+
+create table nhl.teams (
+    team_id varchar(3),
+    name varchar(35),
+    nickname varchar(35),
+    primary key(team_id)
+);
+
+create table nhl.games (
+	season integer,
+    game_id integer,
+    date date,
+    home_team_id varchar references nhl.teams(team_id),
+    away_team_id varchar references nhl.teams(team_id),
+    home_team_score integer,
+    away_team_score integer,
+    rl boolean,
+    gcl boolean,
+    gcll boolean,
+    bs varchar(35),
+    bsc varchar(35),
+    gs integer,
+    primary key(game_id)
+);
+
+create table nhl.events (
+    event_id integer,
+    formal_event_id varchar(15),
+    game_id integer references nhl.games(game_id),
+    period integer,
+    strength integer,
+    type varchar(15),
+    shot_type varchar(15),
+    description varchar(255),
+    player_id integer,
+    team_id varchar references nhl.teams(team_id),
+    xcoord integer,
+    ycoord integer,
+    home_score integer,
+    away_score integer,
+    home_sog integer,
+    away_sog integer,
+    time varchar(10),
+    video_url varchar(255),
+    altvideo_url varchar(255),
+    goalie_id integer,
+    primary key (game_id, event_id)
+);
+
+create table nhl.events_players (
+    game_id integer references nhl.games(game_id),
+    event_id integer,
+    which varchar(15),
+    player_id integer,
+    foreign key(game_id, event_id) references nhl.events(game_id, event_id)
+);
+
+create table nhl.events_penaltybox (
+    game_id integer references nhl.games(game_id),
+    event_id integer,
+    which varchar(15),
+    player_id integer,
+    foreign key(game_id, event_id) references nhl.events(game_id, event_id)
+);
+
+/* USED BY stats.py */
+
+create table nhl.players (
+    player_id integer,
+    jersey integer,
+    name varchar(100),
+    team_id varchar(3),
+    pos varchar(3),
+    dob date,
+    birthcity varchar(100),
+    state varchar(10),
+    country varchar(10),
+    height integer,
+    weight integer,
+    shoots char,
+    primary key(player_id)
+);
+
+create table nhl.stats_skaters_summary (
+    player_id integer,
+    season integer,
+    gp integer,
+    g integer,
+    a integer,
+    p integer,
+    plusminus integer,
+    pim integer,
+    ppg integer,
+    ppp integer,
+    shg integer,
+    shp integer,
+    gw integer,
+    ot integer,
+    s integer,
+    s_pct numeric,
+    toi_g numeric,
+    sft_g numeric,
+    fo_pct numeric,
+    primary key (player_id, season)
+);
+
+create table nhl.stats_skaters_timeonice (
+    player_id integer,
+    season integer,
+    gp integer,
+    evenstrength numeric,
+    evenstrength_g numeric,
+    shorthanded numeric,
+    shorthanded_g numeric,
+    powerplay numeric,
+    powerplay_g numeric,
+    total numeric,
+    total_g numeric,
+    shifts integer,
+    total_s numeric,
+    shifts_g numeric,
+    primary key (player_id, season)
+);
+
+create table nhl.stats_skaters_faceoff (
+    player_id integer,
+    season integer,
+    gp integer,
+    evenstrength_fow integer,
+    evenstrength_fol integer,
+    powerplay_fow integer,
+    powerplay_fol integer,
+    shorthanded_fow integer,
+    shorthanded_fol integer,
+    home_fow integer,
+    home_fol integer,
+    road_fow integer,
+    road_fol integer,
+    fow integer,
+    fol integer,
+    total integer,
+    primary key (player_id, season)
+);
+
+create table nhl.stats_goalies_summary (
+    player_id integer,
+    season integer,
+    gp integer,
+    gs integer,
+    w integer,
+    l integer,
+	ot integer,
+	sa integer,
+	ga integer,
+	gaa real,
+	sv integer,
+	sv_pct numeric,
+	so integer,
+	g integer,
+	a integer,
+	pim integer,
+	toi numeric,
+    primary key (player_id, season)
+);
+
+create index idx_events_game_id on events(game_id);
+create index idx_events_team_id on events(team_id);
+create index idx_events_player_id on events(player_id);
+create index idx_events_players_player_id on events_players(player_id);
+create index idx_events_penaltybox_player_id on events_penaltybox(player_id);
---
+++ nhl/stats.py
@@ -0,0 +1,221 @@
+#!/sw/bin/python2.7
+
+from pprint import pprint
+from BeautifulSoup import BeautifulSoup
+
+import re
+import json
+import urllib2
+import urlparse
+import sqlalchemy
+import ConfigParser
+import datetime
+import time
+import sys
+import getopt
+import os
+
+# Usage information
+def usage():
+    #print "usage information available on github wiki @ https://github.com/wellsoliver/py-nhl/wiki"
+    print "-s <season> \n-v <summary | bios> \n-p <S | G>"
+    raise SystemExit
+
+
+# Grabs a URL and returns a BeautifulSoup object
+def fetchsoup(url, **kwargs):
+    try:
+        if 'verbose' in kwargs: print url
+        res = urllib2.urlopen(url)
+        return BeautifulSoup(res.read())
+    except:
+        return None
+
+
+def processsoup(soup, position, view, tablename, season, conn):
+    table = soup.find('table', 'data')
+    tablerows = table.find('tbody').findAll('tr')
+    
+    view = view.lower();
+    
+    for row in tablerows:
+        cellvalues = row.findAll('td')
+        try:
+            player_id = re.search('=(\d+)$', cellvalues[1].find('a')['href']).groups()[0]
+        except:
+            print 'cannot get player ID from row'
+            continue
+        
+        # Get the list of values we want based on the view
+        if view == 'summary' and position == 'S':
+            values = [cell.text.replace(',', '') for cell in cellvalues[4:]]
+        elif view == 'summary' and position == 'G':
+            values = [cell.text.replace(',', '') for cell in cellvalues[3:]]
+        elif view == 'timeonice':
+            values = [cell.text.replace(',', '') for cell in cellvalues[4:]]
+        elif view == 'faceoffpercentageall':
+            values = [cell.text.replace(',', '') for cell in cellvalues[4:]]
+            values.pop(9)
+            values.pop(11)
+            values.pop()
+        elif view == 'bios' or view == 'goaliebios':
+            values = [cell.text for cell in cellvalues[:-8]]
+            # sometimes NHL.com lists a player on two pages... *shrug*
+            query = 'SELECT * FROM players WHERE player_id = %s'
+            result = conn.execute(query, [player_id])
+            if result.rowcount > 0: continue
+
+        if view in ['summary', 'timeonice', 'faceoffpercentageall']:
+            # sometimes NHL.com lists a player on two pages... *shrug*
+            query = 'DELETE FROM %s WHERE player_id = %s AND season = %s' % (tablename, player_id, season)
+            conn.execute(query)
+
+        # Convert times to decimals
+        for i, value in enumerate(values):
+            if (':' in value):
+                valuelist = [int(x) for x in value.split(':')]
+                values[i] = round(valuelist[0] + (valuelist[1] / 60.0), 2)
+
+        # Custom handling
+        if view in ['bios','goaliebios']:
+            # Create a datetime object for DOB
+            try:
+                idx = 4 if view == 'bios' else 3
+                dobstruct = time.strptime(values[idx], '%b %d \'%y')
+                values[idx] = datetime.datetime.fromtimestamp(time.mktime(dobstruct))
+            except:
+                idx = 4 if view == 'bios' else 3
+                values[idx] = None
+            
+            # Set a position for a goalie
+            if view == 'goaliebios':
+                values.insert(3, 'G')
+
+            # TODO Get the team ID for the current team
+            # teamabbr = cellvalues[2].text.replace(' ', '').split(',')[-1]
+            #values[2] = None
+
+            # Extra fields not in the schema
+            del values[13]
+            del values[12]
+            del values[11]
+
+        elif view == 'summary' and position == 'S' and season in [2005,2006,20052006]:
+            # Game-tying goals removed for 2004/2005, removing it for prior years
+            del values[11]
+
+        # Convert emptys to null
+        for i, value in enumerate(values):
+            if type(value) is unicode and len(value) == 0: value = None
+            values[i] = value
+
+        # Insert season
+        if view in ['summary', 'timeonice', 'faceoffpercentageall']: values.insert(0, season)
+        # Insert player ID
+        values.insert(0, player_id)
+        
+        query = 'INSERT INTO %s VALUES(%s)' % (tablename, ','.join(['%s'] * len(values)))
+        conn.execute(query, values)
+
+
+def main():
+    pwd = os.path.dirname(__file__)
+    if pwd == '': pwd = '.'
+    config = ConfigParser.ConfigParser()
+    config.readfp(open('%s/py-nhl.ini' % pwd))
+    
+    SEASON = False
+    VIEW = False
+    POSITION = 'S'
+
+    positions = ['S', 'G']
+
+    # Key is the NHL.com URL piece, value is the local DB table
+    views = {
+        'S': {
+            'faceOffPercentageAll': 'stats_skaters_faceoff',
+            'bios': 'players',
+            'timeOnIce': 'stats_skaters_timeonice',
+            'summary': 'stats_skaters_summary'
+        }, 'G': {
+            'summary': 'stats_goalies_summary',
+            'goalieBios': 'players',
+        }
+    }
+    
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "s:v:p:", ["season=","view=", "position="])
+    except getopt.GetoptError, e:
+        usage()
+
+    for o, a in opts:
+        if o in ('-s', '--season'): SEASON = int(a)
+        elif o in ('-p', '--position'): POSITION = a
+        elif o in ('-v', '--view'): VIEW = a
+
+    if POSITION not in positions:
+        print 'invalid position %s' % POSITION
+        usage()
+        raise SystemExit
+    if VIEW and VIEW not in views[POSITION].keys():
+        print 'invalid view %s' % VIEW
+        usage()
+        raise SystemExit
+    
+    if SEASON is False:
+        usage()
+
+    if VIEW:
+        runviews = [VIEW]
+    else:
+        runviews = views[POSITION].keys()
+
+    try:
+        ENGINE = config.get('database', 'engine')
+        HOST = config.get('database', 'host')
+        DATABASE = config.get('database', 'database')
+
+        USER = None if not config.has_option('database', 'user') else config.get('database', 'user')
+        SCHEMA = None if not config.has_option('database', 'schema') else config.get('database', 'schema')
+        PASSWORD = None if not config.has_option('database', 'password') else config.get('database', 'password')
+
+    except ConfigParser.NoOptionError:
+        print 'Need to define engine, user, password, host, and database parameters'
+        raise SystemExit
+
+    if USER and PASSWORD: string = '%s://%s:%s@%s/%s' % (ENGINE, USER, PASSWORD, HOST, DATABASE)
+    else:  string = '%s://%s/%s' % (ENGINE, HOST, DATABASE)
+
+    try:
+        db = sqlalchemy.create_engine(string)
+        conn = db.connect()
+    except:
+        print 'Cannot connect to database'
+        raise SystemExit
+
+    if SCHEMA: conn.execute('SET search_path TO %s' % SCHEMA)
+    
+    for view in runviews:
+        tablename = views[POSITION][view]
+        if view not in ['bios', 'goalieBios']:
+            conn.execute("DELETE FROM %s WHERE season = %s" % (tablename, SEASON))
+
+        # position S/G
+        url = 'http://www.nhl.com/ice/playerstats.htm?season=%s&position=%s&gameType=2&viewName=%s' % (SEASON, POSITION, view)
+        soup = fetchsoup(url, verbose=True)
+        if soup: processsoup(soup, POSITION, view, tablename, SEASON, conn)
+        else: continue
+    
+        # Get the max # of pages
+        div = soup.find('div', 'pages')
+        maxpage = int(urlparse.parse_qs(div.findAll('a')[-1]['href'])['pg'][0])
+
+        # Iterate from page 2 through the end
+        for page in xrange(2, maxpage + 1):
+            url = 'http://www.nhl.com/ice/playerstats.htm?season=%s&position=%s&gameType=2&viewName=%s&pg=%s' % (SEASON, POSITION, view, page)
+            soup = fetchsoup(url, verbose=True)
+            if soup: processsoup(soup, POSITION, view, tablename, SEASON, conn)
+
+
+if __name__ == '__main__':
+    main()
